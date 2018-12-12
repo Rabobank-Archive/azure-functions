03b2d492ee79c352be94b6385ec48d1333da541f
Denny van Kleef <dennyvankleef@gmail.com>
2018-12-12 16:08:52 +0100

    Fixed Binding by injecting VstsRestClient and LoganalyticsRestclient

--- VstsLogAnalytics/VstsLogAnalyticsFunction/SecurityScanFunction.cs
+++ VstsLogAnalytics/VstsLogAnalyticsFunction/SecurityScanFunction.cs
@@ -6,26 +6,29 @@ using Newtonsoft.Json;
 using SecurePipelineScan.Rules;
 using SecurePipelineScan.VstsService;
 using VstsLogAnalytics.Client;
+using VstsLogAnalytics.Common;
 
 namespace VstsLogAnalyticsFunction
 {
     public static class SecurityScanFunction
     {
         [FunctionName("SecurityScanFunction")]
-        public static async Task Run([TimerTrigger("0 */30 * * * *")] TimerInfo timerInfo, ILogAnalyticsClient logAnalyticsClient,
-            IVstsRestClient vstsClientObject, ILogger log)
+        public static async Task Run([TimerTrigger("0 */30 * * * *")] TimerInfo timerInfo, 
+            [Inject]ILogAnalyticsClient logAnalyticsClient,
+            [Inject]IVstsRestClient client, 
+            ILogger log)
         {
             try
             {
                 log.LogInformation($"Security scan timed check start: {DateTime.Now}");
 
-                var response = vstsClientObject.Execute(SecurePipelineScan.VstsService.Requests.Project.Projects());
+                var response = client.Execute(SecurePipelineScan.VstsService.Requests.Project.Projects());
                 var projects = response.Data;
                 
 
                 log.LogInformation($"Projects found: {projects.Count}");
 
-                var securityReportScan = new SecurityReportScan(vstsClientObject);
+                var securityReportScan = new SecurityReportScan(client);
 
                 foreach (var project in projects.Value)
                 {
