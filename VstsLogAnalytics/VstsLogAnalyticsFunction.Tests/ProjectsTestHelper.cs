6f3a1e0bae08e104bff18c6e9d2c57a5909279aa
Denny van Kleef <denny.van.kleef@rabobank.nl>
2019-01-08 21:06:01 +0100

    Added ProjectTestHelper for easy creation of projects, added tests for SecurityScanStart

--- VstsLogAnalytics/VstsLogAnalyticsFunction.Tests/ProjectsTestHelper.cs
+++ VstsLogAnalytics/VstsLogAnalyticsFunction.Tests/ProjectsTestHelper.cs
@@ -1,4 +1,43 @@
-$HEADER$namespace $NAMESPACE$
+using System.Collections.Generic;
+using SecurePipelineScan.VstsService.Response;
+using Response = SecurePipelineScan.VstsService.Response;
+
+
+namespace VstsLogAnalyticsFunction.Tests
 {
-  public class $CLASS$ {$END$}
+    public class ProjectsTestHelper
+    {
+        public static Project CreateProjectWithParameters(string name, string id, string description, string url )
+        {
+            var project = new Project
+            {
+                Name = name,
+                Id = id, 
+                Description = description,
+                Url = url
+            };
+
+            return project;
+        }
+        
+        public static Project CreateDummyProject()
+        {
+            var project = new Project
+            {
+                Name = "Dummy project",
+                Id = "1234", 
+                Description = "Describe project",
+                Url = "www.url.com"
+            };
+
+            return project;
+        }
+
+        public static Multiple<Project> CreateMultipleProjectsResponse()
+        {
+
+            var projects = new Multiple<Project>(CreateDummyProject(), CreateDummyProject());
+            return projects;
+        }
+    }
 }
