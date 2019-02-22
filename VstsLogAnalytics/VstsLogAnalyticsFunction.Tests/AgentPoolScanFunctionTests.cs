d7b0ca08f1f5afdd2f6d65974d22d1429a52f79d
Geert van der Cruijsen <geert.van.der.cruijsen@rabobank.nl>
2019-02-22 15:02:44 +0100

    removed unused code

--- VstsLogAnalytics/VstsLogAnalyticsFunction.Tests/AgentPoolScanFunctionTests.cs
+++ VstsLogAnalytics/VstsLogAnalyticsFunction.Tests/AgentPoolScanFunctionTests.cs
@@ -172,20 +172,5 @@ namespace VstsLogAnalyticsFunction.Tests
             //check if reimage called
             Assert.Equal(1, mockHttp.GetMatchCount(postRequest));
         }
-
-        internal class HttpMessageHandlerStub : HttpMessageHandler
-        {
-            private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _sendAsync;
-
-            public HttpMessageHandlerStub(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> sendAsync)
-            {
-                _sendAsync = sendAsync;
-            }
-
-            protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-            {
-                return await _sendAsync(request, cancellationToken);
-            }
-        }
     }
 }
