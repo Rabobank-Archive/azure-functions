a094e9b9686cb6b8e0a33b6d89fc579b98fce6ce
Manuel Riezebosch <mriezebosch@gmail.com>
2019-05-24 13:55:27 +0200

    feat(service hooks): update service hooks from azure function

---
+++ Functions.Tests/ServiceHooksSubscriptionsTests.cs
@@ -0,0 +1,138 @@
+using AutoFixture;
+using Moq;
+using SecurePipelineScan.VstsService;
+using SecurePipelineScan.VstsService.Requests;
+using SecurePipelineScan.VstsService.Response;
+using Xunit;
+using Project = SecurePipelineScan.VstsService.Response.Project;
+
+namespace Functions.Tests
+{
+    public class ServiceHooksSubscriptionsTests
+    {
+        private const string StorageAccountConnectionString = "DefaultEndpointsProtocol=https;AccountName=azdocompliancyqueuedev;AccountKey=aG9pCg==;EndpointSuffix=core.windows.net";
+        private const string AccountName = "azdocompliancyqueuedev";
+
+        [Fact]
+        public void NoSubscriptions_HooksCreated()
+        {
+            // Arrange 
+            var fixture = new Fixture();
+            var client = new Mock<IVstsRestClient>();
+            client
+                .Setup(x => x.Get(It.IsAny<IVstsRequest<Multiple<Project>>>()))
+                .Returns(fixture.Create<Multiple<Project>>())
+                .Verifiable();
+            client
+                .Setup(x => x.Get(It.IsAny<IVstsRequest<Multiple<Hook>>>()))
+                .Returns(fixture.Create<Multiple<Hook>>())
+                .Verifiable();
+
+            // Act
+            var function = new ServiceHooksSubscriptions(
+                new EnvironmentConfig { StorageAccountConnectionString =  StorageAccountConnectionString }, 
+                client.Object);
+
+            function.Run(null);
+
+            // Assert
+            client
+                .Verify(x => x.Post(
+                    It.IsAny<IVstsRequest<Hooks.Add.Body, Hook>>(), 
+                    It.Is<Hooks.Add.Body>(b => 
+                        b.ConsumerInputs.QueueName == "buildcompleted" &&
+                        b.ConsumerInputs.AccountName == AccountName && 
+                        b.ConsumerInputs.AccountKey == "aG9pCg==")));
+
+            client
+                .Verify(x => x.Post(
+                    It.IsAny<IVstsRequest<Hooks.Add.Body, Hook>>(), 
+                    It.Is<Hooks.Add.Body>(b => 
+                        b.ConsumerInputs.QueueName == "releasedeploymentcompleted" &&
+                        b.ConsumerInputs.AccountName == AccountName &&
+                        b.ConsumerInputs.AccountKey == "aG9pCg==")));
+        }
+        
+        [Fact]
+        public void SkipBuildCompletedHook_WhenAlreadySubscribed()
+        {
+            // Arrange 
+            var fixture = new Fixture();
+            fixture
+                .Customize<Project>(ctx => ctx.With(p => p.Id, "project-id"));
+            
+            fixture.Customize<PublisherInputs>(ctx => ctx
+                .With(h => h.ProjectId, "project-id"));
+            fixture.Customize<ConsumerInputs>(ctx => ctx
+                .With(h => h.QueueName, "buildcompleted")
+                .With(h => h.AccountName, AccountName));
+            fixture.Customize<Hook>(ctx => ctx
+                .With(h => h.EventType, "build.complete"));
+            
+            var client = new Mock<IVstsRestClient>();
+            client
+                .Setup(x => x.Get(It.IsAny<IVstsRequest<Multiple<Project>>>()))
+                .Returns(fixture.Create<Multiple<Project>>())
+                .Verifiable();
+            client
+                .Setup(x => x.Get(It.IsAny<IVstsRequest<Multiple<Hook>>>()))
+                .Returns(fixture.Create<Multiple<Hook>>())
+                .Verifiable();
+            
+            // Act
+            var function = new ServiceHooksSubscriptions(
+                new EnvironmentConfig { StorageAccountConnectionString =  StorageAccountConnectionString }, 
+                client.Object);
+
+            function.Run(null);
+
+            // Assert
+            client.Verify();
+            client.Verify(x => x.Post(
+                    It.IsAny<IVstsRequest<Hooks.Add.Body, Hook>>(), 
+                    It.Is<Hooks.Add.Body>(b => b.EventType == "build.complete")), 
+                Times.Never());
+        }
+        
+        [Fact]
+        public void SkipReleaseDeploymentCompletedHook_WhenAlreadySubscribed()
+        {
+            // Arrange 
+            var fixture = new Fixture();
+            fixture
+                .Customize<Project>(ctx => ctx.With(p => p.Id, "project-id"));
+            
+            fixture.Customize<PublisherInputs>(ctx => ctx
+                .With(h => h.ProjectId, "project-id"));
+            fixture.Customize<ConsumerInputs>(ctx => ctx
+                .With(h => h.QueueName, "releasedeploymentcompleted")
+                .With(h => h.AccountName, AccountName));
+            fixture.Customize<Hook>(ctx => ctx
+                .With(h => h.EventType, "ms.vss-release.deployment-completed-event"));
+            
+            var client = new Mock<IVstsRestClient>();
+            client
+                .Setup(x => x.Get(It.IsAny<IVstsRequest<Multiple<Project>>>()))
+                .Returns(fixture.Create<Multiple<Project>>())
+                .Verifiable();
+            client
+                .Setup(x => x.Get(It.IsAny<IVstsRequest<Multiple<Hook>>>()))
+                .Returns(fixture.Create<Multiple<Hook>>())
+                .Verifiable();
+            
+            // Act
+            var function = new ServiceHooksSubscriptions(
+                new EnvironmentConfig { StorageAccountConnectionString =  StorageAccountConnectionString }, 
+                client.Object);
+
+            function.Run(null);
+
+            // Assert
+            client.Verify();
+            client.Verify(x => x.Post(
+                    It.IsAny<IVstsRequest<Hooks.Add.Body, Hook>>(), 
+                    It.Is<Hooks.Add.Body>(b => b.EventType == "ms.vss-release.deployment-completed-event")), 
+                Times.Never());
+        }
+    }
+}
---
+++ Functions/ServiceHooksSubscriptions.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Functions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure.Storage;
+using SecurePipelineScan.VstsService;
+using SecurePipelineScan.VstsService.Requests;
+using SecurePipelineScan.VstsService.Response;
+using Project = SecurePipelineScan.VstsService.Requests.Project;
+
+public class ServiceHooksSubscriptions
+{
+    private readonly EnvironmentConfig _config;
+    private readonly IVstsRestClient _client;
+
+    public ServiceHooksSubscriptions(EnvironmentConfig config, IVstsRestClient client)
+    {
+        _config = config;
+        _client = client;
+    }
+
+    [FunctionName(nameof(ServiceHooksSubscriptions))]
+    public void Run([TimerTrigger("0 0 7-19 * * *", RunOnStartup=false)]TimerInfo trigger)
+    {
+        // This only works because we use the account name and account key in the connection string.
+        var storage = CloudStorageAccount.Parse(_config.StorageAccountConnectionString);
+        var key = Convert.ToBase64String(storage.Credentials.ExportKey());
+
+        UpdateServiceSubscriptions(storage.Credentials.AccountName, key);
+    }
+
+    private void UpdateServiceSubscriptions(string accountName, string accountKey)
+    {
+        var hooks = _client.Get(Hooks.Subscriptions());
+        foreach (var project in _client.Get(Project.Projects()))
+        {
+            AddHookIfNotSubscribed(
+                Hooks.AddHookSubscription(), 
+                Hooks.Add.BuildCompleted(accountName, accountKey,"buildcompleted", project.Id),
+                hooks);
+
+            AddHookIfNotSubscribed(
+                Hooks.AddReleaseManagementSubscription(),
+                Hooks.Add.ReleaseDeploymentCompleted(accountName, accountKey, "releasedeploymentcompleted", project.Id),
+                hooks);
+        }
+    }
+
+    private void AddHookIfNotSubscribed(IVstsRequest<Hooks.Add.Body, Hook> request, Hooks.Add.Body hook, IEnumerable<Hook> hooks)
+    {
+        if (!hooks.Any(h => h.EventType == hook.EventType &&
+                            h.ConsumerInputs.AccountName == hook.ConsumerInputs.AccountName &&
+                            h.PublisherInputs.ProjectId == hook.PublisherInputs.ProjectId))
+        {
+            _client.Post(request, hook);    
+        }
+    }
+}
