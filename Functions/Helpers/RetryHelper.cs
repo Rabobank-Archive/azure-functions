cdc6a47b50d79ce2a2653e200882b43ef6d7bf4a
Brouwer, R (Rik) <Rik.Brouwer@rabobank.com>
2019-06-06 14:18:20 +0200

    Add retryhelper for invalid document version exception. Use retryhelper in buildcompletedscan and releasedeploymentscan.

---
+++ Functions/Helpers/RetryHelper.cs
@@ -0,0 +1,13 @@
+﻿using Polly;
+using Polly.Retry;
+using SecurePipelineScan.VstsService;
+
+namespace Functions.Helpers
+{
+    public class RetryHelper
+    {
+        public static readonly RetryPolicy InvalidDocumentVersionPolicy = Policy
+            .Handle<VstsException>(ex => ex.Message.Contains("InvalidDocumentVersionException"))
+            .Retry(3);
+    }
+}
