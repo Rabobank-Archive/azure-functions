109cc5e64368d6d9cc2ddeef5b12b4b32c26f975
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-08-01 08:58:26 +0200

    Added test to verify what happens when completeness log does not exist (yet)

--- CompletenessCheckFunction.Tests/Activities/GetCompletedScansFromLogAnalyticsActivityTests.cs
+++ CompletenessCheckFunction.Tests/Activities/GetCompletedScansFromLogAnalyticsActivityTests.cs
@@ -1,23 +1,32 @@
-﻿using NSubstitute;
+﻿using System;
 using System.Threading.Tasks;
+using AutoFixture;
+using AutoFixture.AutoNSubstitute;
 using CompletenessCheckFunction.Activities;
 using LogAnalytics.Client;
-using Xunit;
-using Microsoft.Azure.WebJobs;
-using AutoFixture;
 using LogAnalytics.Client.Response;
+using Microsoft.Azure.WebJobs;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using Shouldly;
+using Xunit;
 
 namespace CompletenessCheckFunction.Tests.Activities
 {
     public class GetCompletedScansFromLogAnalyticsActivityTests
     {
+        private readonly Fixture _fixture;
+        public GetCompletedScansFromLogAnalyticsActivityTests()
+        {
+            _fixture = new Fixture();
+            _fixture.Customize(new AutoNSubstituteCustomization());
+        }
+
         [Fact]
         public async Task ShouldQueryLogAnalytics()
         {
             // Arrange
-            var fixture = new Fixture();
-            var response = fixture.Create<LogAnalyticsQueryResponse>();
+            var response = _fixture.Create<LogAnalyticsQueryResponse>();
 
             var client = Substitute.For<ILogAnalyticsClient>();
             client.QueryAsync("").ReturnsForAnyArgs(response);
@@ -34,15 +43,14 @@ namespace CompletenessCheckFunction.Tests.Activities
         public async Task ShouldRetrieveUniqueInstanceIdsFromQueryResponse()
         {
             // Arrange
-            var instanceIds = new object[][]
+            var instanceIds = new[]
             {
-                new object[] { "1" },
-                new object[] { "2" },
-                new object[] { "3" },
-                new object[] { "4" }
+                new object[] {"1"},
+                new object[] {"2"},
+                new object[] {"3"},
+                new object[] {"4"}
             };
-            var fixture = new Fixture();
-            var response = fixture.Create<LogAnalyticsQueryResponse>();
+            var response = _fixture.Create<LogAnalyticsQueryResponse>();
             response.tables[0].rows = instanceIds;
 
             var client = Substitute.For<ILogAnalyticsClient>();
@@ -54,6 +62,21 @@ namespace CompletenessCheckFunction.Tests.Activities
 
             // Assert
             result.Count.ShouldBe(4);
+        }
+
+        [Fact]
+        public async Task ShouldReturnEmptyListWhenLogDoesNotExist()
+        {
+            // Arrange
+            var client = Substitute.For<ILogAnalyticsClient>();
+            client.QueryAsync(Arg.Any<string>()).Throws(new Exception());
+
+            // Act
+            var fun = new GetCompletedScansFromLogAnalyticsActivity(client);
+            var result = await fun.RunAsync(Substitute.For<DurableActivityContextBase>());
+
+            // Assert
+            result.Count.ShouldBe(0);
         }
     }
 }
