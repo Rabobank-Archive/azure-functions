17020ac8419b0585533dabbc8dbbe5e4a7fa3aef
kleefd <denny.van.kleef@rabobank.nl>
2019-08-07 14:59:35 +0200

    using Param to execute script with parameters

--- Scripts/createSpnAndAssignRoles.ps1
+++ Scripts/createSpnAndAssignRoles.ps1
@@ -1,34 +1,16 @@
-$aadIdentifierUri = "http://compliancycheckercompleteness.tas"
-$homepageUrl = "http://localhost:3000/login"
-$aadPassword = ConvertTo-SecureString "8dz1s7hq4GqUrasJFg0H35%bbM4UTcGM%*IJdU!fRb$%IjfJkBKX$%zpYw8lkWA" -AsPlainText -Force
-$servicePrincipalDisplayName = "CompliancyCompletenessChecker"
-$subscriptionId = "d65193b5-a500-405c-b6f4-767fb5063969"
-$loganalyticsResourceGroupName = "rg-d02-dev-azuredevopslogsdev-01"
-$loganalytics = "AzureDevOpsLogsDev"
-$scope = "/subscriptions/$( $subscriptionId )/resourceGroups/$( $loganalyticsResourceGroupName )/providers/Microsoft.OperationalInsights/workspaces/$( $loganalytics )"
+param (
+    [string]$aadDisplayName = "",
+    [string]$servicePrincipalDisplayName = "",
+    [string]$subscriptionId = "",
+    [string]$loganalyticsResourceGroupName = "",
+    [string]$loganalytics = "",
+    [string]$scope = "/subscriptions/$( $subscriptionId )/resourceGroups/$( $loganalyticsResourceGroupName )/providers/Microsoft.OperationalInsights/workspaces/$( $loganalytics )"
+)
 
 $ErrorActionPreference = "Stop"
 
-Write-Host "Check if there exists an application with $( $aad-display-name )"
-$appReg = Get-AzADApplication -DisplayName $aad-display-name
-#if ($appReg -eq $null)
-#{
-#    try
-#    {
-#        Write-Host "Going to create app registration with display name $( $aadDisplayName ) and identifier uri $( $aadIdentifierUri )"
-#        $appReg = New-AzADApplication -DisplayName $aadDisplayName -Password $aadPassword -IdentifierUris $aadIdentifierUri -Homepage $homepageUrl -ReplyUrls $homepageUrl -AvailableToOtherTenants $false -EndDate "2099-12-31" -RequiredResourceAccess ./aad-required-permissions-manifest.json -ErrorAction Stop
-#        Write-Host "Created app registration with appId $( $appReg.ApplicationId )"
-#    }
-#    catch
-#    {
-#        Write-Error -Message "Creation of Application fails"
-#    }
-#
-#}
-#else
-#{
-#    Write-Host "Application with name $( $aadDisplayName ) exists, so it won't be created"
-#}
+Write-Host "Check if there exists an application with $( $aadDisplayName )"
+$appReg = Get-AzADApplication -DisplayName $aadDisplayName
 
 Write-Host "Check if there is an exisiting service principal for application with name $( $aadDisplayName ) and ApplicationId $($appReg.ApplicationId)" 
 $sp = Get-AzADServicePrincipal -ApplicationId $appReg.ApplicationId
