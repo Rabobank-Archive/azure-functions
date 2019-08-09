15417a4e3fc95cf16afcb150b576b07fa07f20a9
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-08-09 16:36:43 +0200

    Page requests to status API to avoid out-of-memory exceptions

--- Functions.Tests/Completeness/Activities/GetCompletedOrchestratorsWithNameActivityTests.cs
+++ Functions.Tests/Completeness/Activities/GetCompletedOrchestratorsWithNameActivityTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture;
@@ -5,6 +7,7 @@ using AutoFixture.AutoNSubstitute;
 using AzDoCompliancy.CustomStatus;
 using Functions.Completeness.Activities;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Newtonsoft.Json.Linq;
 using NSubstitute;
 using Shouldly;
@@ -15,6 +18,7 @@ namespace Functions.Tests.Completeness.Activities
     public class GetCompletedOrchestratorsWithNameActivityTests
     {
         private readonly Fixture _fixture;
+        private readonly string _finalContinuationToken = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes("null"));
 
         public GetCompletedOrchestratorsWithNameActivityTests()
         {
@@ -36,7 +40,8 @@ namespace Functions.Tests.Completeness.Activities
             instances[3].Name = "ProjectScanSupervisor";
             
             var client = Substitute.For<DurableOrchestrationClientBase>();
-            client.GetStatusAsync().Returns(instances);
+            client.GetStatusAsync(Arg.Any<DateTime>(), Arg.Any<DateTime>(), Arg.Any<IEnumerable<OrchestrationRuntimeStatus>>(), Arg.Any<int>(), string.Empty)
+                .Returns(new OrchestrationStatusQueryResult { DurableOrchestrationState = instances, ContinuationToken = _finalContinuationToken });
 
             var func = new GetOrchestratorsByNameActivity();
             var instancesToVerify = await func.Run("ProjectScanSupervisor", client);
@@ -66,7 +71,8 @@ namespace Functions.Tests.Completeness.Activities
             instances[6].RuntimeStatus = runtimeStatus;
 
             var client = Substitute.For<DurableOrchestrationClientBase>();
-            client.GetStatusAsync().Returns(instances);
+            client.GetStatusAsync(Arg.Any<DateTime>(), Arg.Any<DateTime>(), Arg.Any<IEnumerable<OrchestrationRuntimeStatus>>(), Arg.Any<int>(), string.Empty)
+                .Returns(new OrchestrationStatusQueryResult { DurableOrchestrationState = instances, ContinuationToken = _finalContinuationToken });
 
             var func = new GetOrchestratorsByNameActivity();
             var instancesToVerify = await func.Run("ProjectScanSupervisor", client);
--- Functions/Completeness/Activities/GetOrchestratorsByNameActivity.cs
+++ Functions/Completeness/Activities/GetOrchestratorsByNameActivity.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
 
 namespace Functions.Completeness.Activities
 {
@@ -20,9 +22,18 @@ namespace Functions.Completeness.Activities
                 OrchestrationRuntimeStatus.Terminated
             };
 
-            return (await client.GetStatusAsync().ConfigureAwait(false))
-                .Where(i => i.Name == name && runtimeStatuses.Contains(i.RuntimeStatus))
-                .ToList();
+            var foundOrchestrators = new List<DurableOrchestrationStatus>();
+            var continuationToken = string.Empty;
+            do
+            {
+                var orchestratorsPage = await client.GetStatusAsync(DateTime.Now.AddDays(-2), DateTime.Now, runtimeStatuses, 1000, continuationToken).ConfigureAwait(false);
+
+                foundOrchestrators.AddRange(orchestratorsPage.DurableOrchestrationState.Where(i => i.Name == name));
+
+                continuationToken = orchestratorsPage.ContinuationToken;
+            } while (Encoding.UTF8.GetString(Convert.FromBase64String(continuationToken)) != "null");  // It's magic :)
+
+            return foundOrchestrators;
         }
     }
 }
