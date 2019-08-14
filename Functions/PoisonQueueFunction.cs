c1335852cd5f3825a0d66a3bb3378513ea387ad7
Verhaar, CA (Kees) <kees.verhaar@rabobank.nl>
2019-08-14 15:55:45 +0200

    Actually delete requeued messages from poison queue and return response with requeued message ids

--- Functions/PoisonQueueFunction.cs
+++ Functions/PoisonQueueFunction.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Net;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
@@ -5,6 +7,7 @@ using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Functions
 {
@@ -18,36 +21,48 @@ namespace Functions
         }
 
         [FunctionName(nameof(PoisonQueueFunction))]
-        public async Task Requeue(
+        public async Task<HttpResponseMessage> Requeue(
             [HttpTrigger(AuthorizationLevel.Anonymous, Route = "poison/requeue/{queue}")]HttpRequestMessage request,
             string queue,
             ILogger log)
         {
-            if (string.IsNullOrEmpty(queue)) return;
+            if (string.IsNullOrEmpty(queue)) return new HttpResponseMessage(HttpStatusCode.BadRequest);
 
             log.LogInformation($"Requeue from: {queue}");
 
             var storage = CloudStorageAccount.Parse(_config.EventQueueStorageConnectionString);
             var client = storage.CreateCloudQueueClient();
 
-            await RequeuePoisonMessages(
+            var requeuedPoisonMessages = await RequeuePoisonMessages(
                 client.GetQueueReference(queue),
                 client.GetQueueReference($"{queue}-poison"),
                 log);
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(
+                    JsonConvert.SerializeObject(new {requeuedMessages = requeuedPoisonMessages}), System.Text.Encoding.UTF8, "application/json")
+            };
         }
 
-        private async Task RequeuePoisonMessages(CloudQueue queue, CloudQueue poison, ILogger log)
+        private async Task<IList<string>> RequeuePoisonMessages(CloudQueue queue, CloudQueue poison, ILogger log)
         {
+            var requeuedMessageIds = new List<string>();
+            
             var message = await poison.GetMessageAsync();
+
             while (message != null)
             {
                 log.LogInformation($"Requeue message with id: {message.Id}");
-                await queue.AddMessageAsync(message);
+                await queue.AddMessageAsync(new CloudQueueMessage(message.AsString));
+                await poison.DeleteMessageAsync(message);
+                requeuedMessageIds.Add(message.Id);
 
                 message = await poison.GetMessageAsync();
             }
 
             log.LogInformation($"Done");
+            return requeuedMessageIds;
         }
     }
 }
