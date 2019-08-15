0e4b98a88dde91e1beb11085a085d95dbe5404ca
kleefd <denny.van.kleef@rabobank.nl>
2019-08-15 11:25:33 +0200

    Check on message 429 for retry

--- Functions/Helpers/RetryHelper.cs
+++ Functions/Helpers/RetryHelper.cs
@@ -23,23 +23,28 @@ namespace Functions.Helpers
         }
 
         public static RetryOptions ActivityRetryOptions => new RetryOptions(
-            firstRetryInterval: TimeSpan.FromSeconds(1 * 60), // First retry happens after 1 minute
-            maxNumberOfAttempts: 6)                           // Maximum of 6 attempts
-        {
-            BackoffCoefficient = 1.5,                         // Back-off timer is multiplied by this number for each retry
-            Handle = IsRetryableActivity,
-            MaxRetryInterval = TimeSpan.FromSeconds(25 * 60), // Maximum time to wait
-            RetryTimeout = TimeSpan.FromSeconds(5 * 60)       // Time to wait before a single retry times out
-        };
+                firstRetryInterval: TimeSpan.FromSeconds(1 * 60), // First retry happens after 1 minute
+                maxNumberOfAttempts: 6) // Maximum of 6 attempts
+            {
+                BackoffCoefficient = 1.5, // Back-off timer is multiplied by this number for each retry
+                Handle = IsRetryableActivity,
+                MaxRetryInterval = TimeSpan.FromSeconds(25 * 60), // Maximum time to wait
+                RetryTimeout = TimeSpan.FromSeconds(5 * 60) // Time to wait before a single retry times out
+            };
 
         private static bool IsRetryableActivity(Exception exception)
         {
-            return (exception.InnerException is FlurlHttpException flurlHttpException && // Handle rate limits (happens if we got blocked by rate limits)
-                    flurlHttpException.Call.HttpStatus == (HttpStatusCode) 429)
-                   || (exception.InnerException is SocketException socketException && // Handle timeout (happens if we got delayed by rate limits)
-                       socketException.Message.Contains(
-                           "A connection attempt failed because the connected party did not properly respond after a period of time")
-                   );
+            return
+                (exception.InnerException is FlurlHttpException
+                     flurlHttpException && // Handle rate limits (happens if we got blocked by rate limits)
+                 (flurlHttpException.Message.Contains("Call failed with status code 429") ||
+                 flurlHttpException.Call.HttpStatus == (HttpStatusCode) 429))
+                || (
+                    exception.InnerException is SocketException
+                        socketException && // Handle timeout (happens if we got delayed by rate limits)
+                    socketException.Message.Contains(
+                        "A connection attempt failed because the connected party did not properly respond after a period of time")
+                );
         }
     }
 }
