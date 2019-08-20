8c52bfcf8e133d288b5567a1987456f83ba94876
kleefd <denny.van.kleef@rabobank.nl>
2019-08-20 10:59:55 +0200

    Created BuildDefinitionsActivity

---
+++ Functions/Activities/BuildDefinitionsActivity.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using SecurePipelineScan.Rules.Security;
+using SecurePipelineScan.VstsService;
+using SecurePipelineScan.VstsService.Requests;
+using SecurePipelineScan.VstsService.Response;
+using Project = SecurePipelineScan.VstsService.Response.Project;
+
+namespace Functions.Activities
+{
+    public class BuildDefinitionsActivity
+    {
+        private readonly IVstsRestClient _azuredo;
+
+        public BuildDefinitionsActivity(IVstsRestClient azuredo)
+        {
+            _azuredo = azuredo;
+        }
+
+        [FunctionName(nameof(BuildDefinitionsActivity))]
+        public List<BuildDefinition> Run([ActivityTrigger] Project project)
+        {
+            var items = _azuredo.Get(Builds.BuildDefinitions(project.Id));
+
+            return items.ToList();
+        }
+    }
+}
