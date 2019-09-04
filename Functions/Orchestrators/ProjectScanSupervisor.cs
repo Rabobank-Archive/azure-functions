a2b92617ac11bc4f71eb26d7d4ca16f81dd91f7d
Brouwer, R (Rik) <Rik.Brouwer@rabobank.com>
2019-09-04 14:21:03 +0200

    add random timer to project scan orchestrator

--- Functions/Orchestrators/ProjectScanSupervisor.cs
+++ Functions/Orchestrators/ProjectScanSupervisor.cs
@@ -5,23 +5,36 @@ using System.Collections.Generic;
 using System.Linq;
 using Functions.Helpers;
 using Task = System.Threading.Tasks.Task;
+using System.Threading;
+using System;
 
 namespace Functions.Orchestrators
 {
     public class ProjectScanSupervisor
     {
+        private const int MaxSleep = 300; 
+
         [FunctionName(nameof(ProjectScanSupervisor))]
         public async Task RunAsync([OrchestrationTrigger] DurableOrchestrationContextBase context)
         {
             var projects = context.GetInput<List<Project>>();
             context.SetCustomStatus(new SupervisorOrchestrationStatus { TotalProjectCount = projects.Count });
 
+            await Task.WhenAll(projects.Select(async p => await StartProjectScanOrchestratorWithTimeTriggerAsync(context, p)));
+                
             //No multithreading with linq, so we won't hit rate limits
-            foreach (var project in projects)
-            {
-                await context.CallSubOrchestratorAsync(nameof(ProjectScanOrchestration),
-                    OrchestrationIdHelper.CreateProjectScanOrchestrationId(context.InstanceId, project.Id), project);
-            }
+            //foreach (var project in projects)
+            //{
+            //    await context.CallSubOrchestratorAsync(nameof(ProjectScanOrchestration),
+            //        OrchestrationIdHelper.CreateProjectScanOrchestrationId(context.InstanceId, project.Id), project);
+            //}
+        }
+
+        private async static Task StartProjectScanOrchestratorWithTimeTriggerAsync(DurableOrchestrationContextBase context, Project project)
+        {
+            await context.CreateTimer(DateTime.Now.AddMinutes(new Random().Next(MaxSleep)), CancellationToken.None);
+            await context.CallSubOrchestratorAsync(nameof(ProjectScanOrchestration),
+                OrchestrationIdHelper.CreateProjectScanOrchestrationId(context.InstanceId, project.Id), project);
         }
     }
 }
