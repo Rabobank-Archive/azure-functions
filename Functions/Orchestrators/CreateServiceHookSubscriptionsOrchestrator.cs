3f7a87bdff779dbd35a3bb8b351aaaf976d7f7b4
Manuel Riezebosch <mriezebosch@gmail.com>
2019-09-09 16:32:39 +0200

    feat(create service hook): execute orchestration in parallel

--- Functions/Orchestrators/CreateServiceHookSubscriptionsOrchestrator.cs
+++ Functions/Orchestrators/CreateServiceHookSubscriptionsOrchestrator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Azure.WebJobs;
 using Response = SecurePipelineScan.VstsService.Response;
 using System.Threading.Tasks;
@@ -13,21 +14,21 @@ namespace Functions.Orchestrators
         [FunctionName(nameof(CreateServiceHookSubscriptionsOrchestrator))]
         public async Task RunAsync([OrchestrationTrigger] DurableOrchestrationContextBase context)
         {
-            var projects =
-                await context.CallActivityWithRetryAsync<IList<Response.Project>>(nameof(GetProjectsActivity),
-                    RetryHelper.ActivityRetryOptions, null);
-            var existingHooks = await context.CallActivityWithRetryAsync<IList<Response.Hook>>(nameof(GetHooksActivity),
-                RetryHelper.ActivityRetryOptions, null);
+            var projects = await context.CallActivityWithRetryAsync<IList<Response.Project>>(
+                nameof(GetProjectsActivity),
+                RetryHelper.ActivityRetryOptions, 
+                null);
+            var hooks = await context.CallActivityWithRetryAsync<IList<Response.Hook>>(
+                nameof(GetHooksActivity),
+                RetryHelper.ActivityRetryOptions, 
+                null);
 
             await context.CallActivityAsync(nameof(CreateStorageQueuesActivity), null);
-
-            foreach (var p in projects)
-            {
-                await context.CallActivityWithRetryAsync(nameof(CreateServiceHookSubscriptionsActivity),
+            await Task.WhenAll(projects.Select(async p => 
+                await context.CallActivityWithRetryAsync(
+                    nameof(CreateServiceHookSubscriptionsActivity),
                     RetryHelper.ActivityRetryOptions,
-                    new CreateServiceHookSubscriptionsActivityRequest {Project = p, ExistingHooks = existingHooks});
-            }
-                
+                    new CreateServiceHookSubscriptionsActivityRequest {Project = p, ExistingHooks = hooks})));
         }
     }
 }
