f2fb58d4c8c1b0c59d2e951742d64d42fdd597f5
kleefd <denny.van.kleef@rabobank.nl>
2019-09-16 16:43:17 +0200

    Call new LogAnalyticsConfigurationItemsUploadActivity in Orchestrator

---
+++ Functions/Activities/LogAnalyticsConfigurationItemsUploadActivity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using LogAnalytics.Client;
+using Microsoft.Azure.WebJobs;
+using System.Threading.Tasks;
+using Functions.Model;
+
+namespace Functions.Activities
+{
+    public class LogAnalyticsConfigurationItemsUploadActivity
+    {
+        private readonly ILogAnalyticsClient _analytics;
+
+        public LogAnalyticsConfigurationItemsUploadActivity(ILogAnalyticsClient analytics)
+        {
+            _analytics = analytics;
+        }
+
+        [FunctionName(nameof(LogAnalyticsConfigurationItemsUploadActivity))]
+        public async Task RunAsync([ActivityTrigger] IEnumerable<ConfigurationItem> request)
+        {
+        }
+    }
+}
--- Functions/Orchestrators/ConfigurationItemsOrchestration.cs
+++ Functions/Orchestrators/ConfigurationItemsOrchestration.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Functions.Activities;
+using Functions.Model;
 using Microsoft.Azure.WebJobs;
 
 namespace Functions.Orchestrators
@@ -8,8 +11,11 @@ namespace Functions.Orchestrators
         [FunctionName(nameof(ConfigurationItemsOrchestration))]
         public async Task RunAsync([OrchestrationTrigger] DurableOrchestrationContextBase context)
         {
-            
-        }
+            var configurationItems =
+                await context.CallActivityAsync<List<ConfigurationItem>>(
+                    nameof(GetConfigurationItemsFromTableStorageActivity), null);
 
+            await context.CallActivityAsync(nameof(LogAnalyticsConfigurationItemsUploadActivity), configurationItems);
+        }
     }
 }
