aafe1a7bdb7d8b217cadf0221de447bb359ad52f
John Hus <johnhus1991@gmail.com>
2019-10-01 08:35:43 +0200

    Move powershell script (to get storage account key and put it in an variable) to solution and remove inline script in pipeline

---
+++ GetStorageAccountKey.ps1
@@ -0,0 +1,12 @@
+[CmdletBinding()]
+param (
+   $functionsResourceGroupName,
+   $eventQueueStorageAccount
+)
+$ErrorActionPreference = "Stop"
+
+#get storageKey
+Write-Verbose "Get storage key for $(eventQueueStorageAccount)" -verbose
+$storageKey1=(Get-AzureRmStorageAccountKey -ResourceGroupName $(functionsResourceGroupName) -AccountName $(eventQueueStorageAccount)).Value[0]
+Write-Verbose "Writing key to eventQueueStorageConnectionString variable" -verbose
+Write-Host "##vso[task.setvariable variable=eventQueueStorageConnectionString;issecret=true]DefaultEndpointsProtocol=https;AccountName=$(eventQueueStorageAccount);AccountKey=$storageKey1;EndpointSuffix=core.windows.net"
