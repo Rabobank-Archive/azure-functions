fd691892e505994412fd05000b3e514c79627062
kleefd <denny.van.kleef@rabobank.nl>
2019-10-01 15:44:33 +0200

    Fixed script

--- GetStorageAccountKey.ps1
+++ GetStorageAccountKey.ps1
@@ -6,7 +6,7 @@ param (
 $ErrorActionPreference = "Stop"
 
 #get storageKey
-Write-Verbose "Get storage key for $(eventQueueStorageAccount)" -verbose
-$storageKey1=(Get-AzureRmStorageAccountKey -ResourceGroupName $(functionsResourceGroupName) -AccountName $(eventQueueStorageAccount)).Value[0]
-Write-Verbose "Writing key to eventQueueStorageConnectionString variable" -verbose
-Write-Host "##vso[task.setvariable variable=eventQueueStorageConnectionString;issecret=true]DefaultEndpointsProtocol=https;AccountName=$(eventQueueStorageAccount);AccountKey=$storageKey1;EndpointSuffix=core.windows.net"
+Write-Host "Get storage key for $(eventQueueStorageAccount)" -verbose
+$storageKey1=(Get-AzureRmStorageAccountKey -ResourceGroupName $functionsResourceGroupName -AccountName $eventQueueStorageAccount).Value[0]
+Write-Host "Writing key to eventQueueStorageConnectionString variable"
+Write-Host "##vso[task.setvariable variable=eventQueueStorageConnectionString;issecret=true]DefaultEndpointsProtocol=https;AccountName=$eventQueueStorageAccount;AccountKey=$storageKey1;EndpointSuffix=core.windows.net"
