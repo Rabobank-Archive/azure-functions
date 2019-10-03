4cca0ae1d6ec5c53acb9924ce6a2e55e5a122d57
John Hus <johnhus1991@gmail.com>
2019-10-03 08:30:58 +0200

    fix PR comments

--- Functions/Model/ConfigurationItem.cs
+++ Functions/Model/ConfigurationItem.cs
@@ -7,17 +7,19 @@ namespace Functions.Model
         public string CiIdentifier { get; set; }
         public string CiName { get; set; }
         public string BoDepartmentInfo { get; set; }
-        public string BoNiveau1 { get; set; }
-        public string BoNiveau2 { get; set; }
-        public string BoNiveau3 { get; set; }
-        public string BoNiveau4 { get; set; }
-        public string BoNiveau5 { get; set; }
+        public string BusinessOwnerName { get; set; }
+        public string BoLevel1 { get; set; }
+        public string BoLevel2 { get; set; }
+        public string BoLevel3 { get; set; }
+        public string BoLevel4 { get; set; }
+        public string BoLevel5 { get; set; }
         public string SoDepartmentInfo { get; set; }
-        public string SoNiveau1 { get; set; }
-        public string SoNiveau2 { get; set; }
-        public string SoNiveau3 { get; set; }
-        public string SoNiveau4 { get; set; }
-        public string SoNiveau5 { get; set; }
+        public string SystemOwnerName { get; set; }
+        public string SoLevel1 { get; set; }
+        public string SoLevel2 { get; set; }
+        public string SoLevel3 { get; set; }
+        public string SoLevel4 { get; set; }
+        public string SoLevel5 { get; set; }
 
     }
 }
