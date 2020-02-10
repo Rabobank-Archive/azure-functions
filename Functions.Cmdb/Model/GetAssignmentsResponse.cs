98b9ae24e37d72110837e7189cc53dbac2aae034
Peter Dol <herrpiet@hotmail.com>
2020-02-10 14:26:06 +0100

    verplaatsen cmdb vanuit rules

---
+++ Functions.Cmdb/Model/GetAssignmentsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Functions.Cmdb.Model
+{
+    [ExcludeFromCodeCoverage]
+    public class GetAssignmentsResponse
+    {
+        public IEnumerable<AssignmentContentItem> Content { get; set; }
+    }
+}
