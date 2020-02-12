f03ea3a39297ada19f936e6a32bcb99b53f7c8e6
Chung Lok Lam <Chung.Lok.Lam@rabobank.nl>
2020-02-12 13:19:31 +0100

    Added Sox lookup table

---
+++ Functions/Security/SoxLookup.cs
@@ -0,0 +1,38 @@
+﻿using SecurePipelineScan.Rules.Security;
+using System.Collections.Generic;
+
+namespace Functions.Security
+{
+    public class SoxLookup
+    {
+        private HashSet<string> SoxRules
+        {
+            get
+            {
+                return new HashSet<string>
+                {
+                    nameof(ArtifactIsStoredSecure),
+                    nameof(NobodyCanBypassPolicies),
+                    nameof(NobodyCanDeleteBuilds),
+                    nameof(NobodyCanDeleteReleases),
+                    nameof(NobodyCanDeleteTheRepository),
+                    nameof(NobodyCanDeleteTheTeamProject),
+                    nameof(NobodyCanManageApprovalsAndCreateReleases),
+                    nameof(PipelineHasAtLeastOneStageWithApproval),
+                    nameof(PipelineHasRequiredRetentionPolicy),
+                    nameof(ProductionStageUsesArtifactFromSecureBranch),
+                    nameof(ReleaseBranchesProtectedByPolicies),
+                    nameof(ReleasePipelineUsesBuildArtifact)
+                };
+            }
+        }
+
+        public bool IsSox(string ruleName)
+        {
+            if (string.IsNullOrEmpty(ruleName))
+                return false;
+
+            return SoxRules.Contains(ruleName);
+        }
+    }
+}
